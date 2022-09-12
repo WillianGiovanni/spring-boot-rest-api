dfea299069234ee2c33ca5fdeed2674a1e0cdb80
WillianGiovanni <willianclemente10@gmail.com>
2022-09-12 15:50:21 -0300

    custom json serialization

--- src/main/java/br/com/williangiovanni/springbootrestapi/data/PersonVO.java
+++ src/main/java/br/com/williangiovanni/springbootrestapi/data/PersonVO.java
@@ -2,17 +2,25 @@ package br.com.williangiovanni.springbootrestapi.data;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({ "id", "address", "first_name", "last_name", "gender" })
 public class PersonVO implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private long id;
 
+    @JsonProperty("first_name")
     private String firstName;
 
+    @JsonProperty("last_name")
     private String lastName;
 
     private String address;
 
+    @JsonIgnore
     private String gender;
 
     public PersonVO() {
